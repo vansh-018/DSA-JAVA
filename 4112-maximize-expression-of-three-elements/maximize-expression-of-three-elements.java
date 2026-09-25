2dc52bbba60397ff5e0ab6f57ca1812af0f9acf8
Vansh18 <vanshvajani2006@gmail.com>
2026-09-25 16:55:14 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (99.43%) - LeetSync

--- 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.java
+++ 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.java
@@ -1,10 +1,21 @@
 class Solution {
     public int maximizeExpressionOfThree(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int a = nums[n-1];
-        int b = nums[n-2];
-        int c = nums[0];
-        return a+b-c;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max1){
+                max2=max1;
+                max1=nums[i];
+            }
+            else if(nums[i]>max2){
+                max2=nums[i];
+            }
+            if(nums[i]<min){
+                min = nums[i];
+            }
+
+        }
+        return max1+max2-min;
     }
 }
